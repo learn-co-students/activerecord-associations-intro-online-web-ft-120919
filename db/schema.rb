e48bddc0976bdb6559bbbb7a2c49ae9d798cc3dd
Felicia_Smith <Smithfn1012@gmail.com>
2020-02-01 15:05:18 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 2020_02_01_211811) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+  end
 
 end
